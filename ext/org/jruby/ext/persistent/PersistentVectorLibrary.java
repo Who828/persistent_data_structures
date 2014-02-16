6ed45fabc6169fe8a1eab5f30fc0c8c1c9162d0e
Smit Shah <who828@gmail.com>
2014-02-16 21:21:42 +0530

    bit partion trie if there more then 32 elements

--- ext/org/jruby/ext/persistent/PersistentVectorLibrary.java
+++ ext/org/jruby/ext/persistent/PersistentVectorLibrary.java
@@ -54,32 +54,39 @@ public class PersistentVectorLibrary implements Library {
 
     public static class Node extends RubyObject {
         transient public AtomicReference<Thread> edit;
-        public IRubyObject array;
+        public RubyArray array;
 
         public Node(Ruby runtime, RubyClass rubyClass) {
             super(runtime, rubyClass);
         }
 
-        public Node initialize_params(ThreadContext context) {
+        public Node initialize_params(ThreadContext context, AtomicReference<Thread> edit) {
+            this.edit = edit;
             this.array = RubyArray.newArray(context.runtime, 32);
+            return this;
+        }
+
+        public Node initialize_params_arry(ThreadContext context, AtomicReference<Thread> edit, RubyArray arry) {
+            this.edit = edit;
+            this.array = arry;
             return this;
         }
     }
 
     @JRubyClass(name="Vector")
     public static class PersistentVector extends RubyObject {
-
-        int cnt;
-        public IRubyObject shift;
-        public  IRubyObject root;
-        public  IRubyObject tail;
+        static AtomicReference<Thread> NOEDIT = new AtomicReference<Thread>(null);
+        public int cnt;
+        public  int shift;
+        public  Node root;
+        public  RubyArray tail;
 
 
        public PersistentVector(Ruby runtime, RubyClass rubyClass) {
             super(runtime, rubyClass);
         }
 
-        public IRubyObject initialize(ThreadContext context, int cnt, IRubyObject shift, IRubyObject root, IRubyObject tail) {
+        public IRubyObject initialize(ThreadContext context, int cnt, int shift, Node root, RubyArray tail) {
             this.cnt = cnt;
             this.shift = shift;
             this.root = root;
@@ -89,9 +96,8 @@ public class PersistentVectorLibrary implements Library {
 
         @JRubyMethod(name = "vector", meta = true)
         static public IRubyObject vector(ThreadContext context, IRubyObject cls, IRubyObject items) {
-            RubyArray arry = RubyArray.newArray(context.runtime);
             PersistentVector ret = new PersistentVector(context.runtime, (RubyClass) cls);
-            PersistentVector ret1 = (PersistentVector) ret.initialize(context, 3, RubyFixnum.newFixnum(context.runtime, 4), new Node(context.runtime, Node).initialize(context), arry);
+            PersistentVector ret1 = (PersistentVector) ret.initialize(context, 0, 5, new Node(context.runtime, Node).initialize_params(context, NOEDIT), RubyArray.newArray(context.runtime));
             for(Object item : (RubyArray) items) {
                 ret1 = (PersistentVector) ret1.add(context, JavaUtil.convertJavaToRuby(context.runtime, item));
             }
@@ -99,16 +105,63 @@ public class PersistentVectorLibrary implements Library {
        }
 
        @JRubyMethod(name = "tail")
-       public IRubyObject tail() {
+       public IRubyObject tail(ThreadContext context) {
            return this.tail;
        }
 
+       private static Node newPath(ThreadContext context, AtomicReference<Thread> edit, int level, Node node) {
+           if (level == 0)
+               return node;
+           Node ret = new Node(context.runtime, Node).initialize_params(context, edit);
+           ret.array.set(0, newPath(context, edit, level-5, node));
+           return  ret;
+       }
+
+        private Node pushTail(ThreadContext context, int level, Node parent, Node tailnode){
+            int subidx = ((cnt - 1) >>> level) & 0x01f;
+            Node ret = new Node(context.runtime, Node).initialize_params_arry(context, parent.edit, (RubyArray) parent.array.dup());
+            Node nodeToInsert;
+            if(level == 5)
+            {
+                nodeToInsert = tailnode;
+            }
+            else
+            {
+                Node child = (Node) parent.array.get(subidx);
+                nodeToInsert = (child != null)?
+                        pushTail(context, level-5,child, tailnode)
+                        :newPath(context, root.edit,level-5, tailnode);
+            }
+            ret.array.set(subidx, nodeToInsert);
+            return ret;
+        }
+
        @JRubyMethod(name = "add", required = 1)
        public IRubyObject add(ThreadContext context, IRubyObject val) {
-           PersistentVector ret = new PersistentVector(context.runtime, getMetaClass());
-           RubyArray newTail =  (RubyArray) tail.dup();
-           newTail.add(val);
-           return ret.initialize(context, this.cnt, this.shift, this.root, newTail);
+           if (cnt - tailoff() < 32) {
+               PersistentVector ret = new PersistentVector(context.runtime, getMetaClass());
+               RubyArray newTail = (RubyArray) tail.dup();
+               newTail.add(val);
+               return ret.initialize(context, this.cnt+1, this.shift, this.root, newTail);
+           }
+
+           Node newroot;
+           Node tailnode = new Node(context.runtime, Node).initialize_params(context, root.edit);
+           int newshift = shift;
+
+           if ((cnt >> 5) > (1 << shift)) {
+               System.out.println("level" + shift);
+               newroot = new Node(context.runtime, Node).initialize_params(context, root.edit);
+               newroot.array.set(0, root);
+               newroot.array.set(1, newPath(context, root.edit, shift, tailnode));
+               newshift += 5;
+           } else
+               newroot = pushTail(context, shift, root, tailnode);
+
+           RubyArray arry = RubyArray.newArray(context.runtime, 1);
+           arry.unshift(val);
+
+           return new PersistentVector(context.runtime, getMetaClass()).initialize(context, cnt + 1, newshift, newroot, arry);
        }
 
         final int tailoff(){
